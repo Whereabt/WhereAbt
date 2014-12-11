2b1a76b64fbec5b3584b831d432024bc7618a4e2
Nicolas Isaza <17nisa@mvcds.org>
2014-12-10 22:56:51 -0500

    typo

---
+++ Whereabout 2/Whereabout 2/WelcomeViewController.h
@@ -0,0 +1,17 @@
+//
+//  WelcomeViewController.h
+//  Whereabout 2
+//
+//  Created by Nicolas on 12/10/14.
+//  Copyright (c) 2014 Nicolas Isaza. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WelcomeViewController : UIViewController
+
+
+//create outlet property for Get Started" button
+@property (weak, nonatomic) IBOutlet UIButton *GetStarted;
+
+@end
