cc3a3c9741198fe0c333f3bdcb72c2dbe8b72afd
Rob Franco <rob-franco>
2014-12-30 00:36:04 -0800

    API for client to submit new photos to the server
    This page accepts the UserID, Latitude, Longitude and Photo URL for a
    new photo as parameters on
    the URL query string. Then the page inserts that into the database.
    
    For a new photo submitted from Seattle, try this example:
    https://n46.org/whereabt/newphoto.php?UserID=Rob&Latitude=47.603569&Long
    itude=-122.329453&PhotoURL=http://1.bp.blogspot.com/-IXsF14WPM_M/UTybWm0
    N-bI/AAAAAAAAfMQ/Oks9PRGs90s/s1600/Seattle+USA+1.jpg

---
+++ Server PHP/newphoto.php
@@ -0,0 +1,44 @@
+
+<?php
+
+    //This page inserts new photo information from the client into the database
+
+    echo "This page accepts query string parameters UserID, Longitude, Latitude and PhotoURL<br /><br />";
+    
+    //Step1 - Connect to the DB using the connection info stored in login.php
+    
+    require_once 'login.php';
+   
+    $db_server = mysql_connect($hostname, $username, $password);
+    
+    if (!$db_server) die("Unable to connect to MySQL: " . mysql_error());
+    
+    mysql_select_db($dbname) or die("Unable to select database: " . mysql_error());
+
+    //Step2 - In this section, we'll set temp variables for the URL query string parameters
+    //We need to set temp variables because the
+    $tempUserID = $_GET['UserID'];
+    $tempLatitude = $_GET['Latitude'];
+    $tempLongitude = $_GET['Longitude'];
+    $tempPhotoURL = $_GET['PhotoURL'];
+
+    //Step3 - **Echo the output to the page, debugging only, users won't see this**
+    echo "Here are the values you submitted<br />";
+    echo $tempUserID . '<br />';
+    echo $tempLatitude . '<br />';
+    echo $tempLongitude . '<br />';
+    echo $tempPhotoURL . '<br /><br />';
+
+    //Step 4 - Build the query string in a temp variable then actually insert the URL query string into
+    //$tempQuery = "INSERT INTO Feed VALUES('$tempUserID','$tempLatitude','$tempLongitude','$tempPhotoURL')";
+    //$result = mysql_query($tempQuery);
+    $result = mysql_query("INSERT INTO Feed VALUES('$tempUserID','$tempLatitude','$tempLongitude','$tempPhotoURL')");
+    
+    if (!$result) die("Database INSERT failed: " . mysql_error());
+
+    echo "Did it work??<br />";
+    echo $result;
+    
+    mysql_close($db_server);
+    
+?>
