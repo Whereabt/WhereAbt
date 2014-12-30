eba4fab97eff88b22149e9b2776ef69fe32d9396
Rob Franco <rob-franco>
2014-12-30 00:20:53 -0800

    Login info for PHP to access test database
    Contains username, password and location of the database for other PHP
    files to reference. This helps us protect the password and if we need
    to change it, we can do it in one place

---
+++ Server PHP/login.php
@@ -0,0 +1,9 @@
+
+<?php
+    //Database connection information
+    
+    $hostname='whereabt.db.8413086.hostedresource.com';
+    $username='whereabt';
+    $password='Seattle2015!!!';
+    $dbname='whereabt';
+ ?>
