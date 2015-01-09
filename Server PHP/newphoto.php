741aa3df77b827591ac87488383081bceef348c5
Rob Franco <rob-franco>
2015-01-09 01:23:37 -0800

    Removed debug echo and clarified comments
    Before it used to return lots of debug junk in the response body, now
    it will not do that

--- Server PHP/newphoto.php
+++ Server PHP/newphoto.php
@@ -3,7 +3,7 @@
 
     //This page inserts new photo information from the client into the database
 
-    echo "This page accepts query string parameters UserID, Longitude, Latitude and PhotoURL<br /><br />";
+    //The page accepts query string parameters UserID, Longitude, Latitude, PhotoURL
     
     //Step1 - Connect to the DB using the connection info stored in login.php
     
@@ -16,29 +16,17 @@
     mysql_select_db($dbname) or die("Unable to select database: " . mysql_error());
 
     //Step2 - In this section, we'll set temp variables for the URL query string parameters
-    //We need to set temp variables because the
+    //If we don't set temp variables, we run into problems with the mysql_query function
     $tempUserID = $_GET['UserID'];
     $tempLatitude = $_GET['Latitude'];
     $tempLongitude = $_GET['Longitude'];
     $tempPhotoURL = $_GET['PhotoURL'];
 
-    //Step3 - **Echo the output to the page, debugging only, users won't see this**
-    echo "Here are the values you submitted<br />";
-    echo $tempUserID . '<br />';
-    echo $tempLatitude . '<br />';
-    echo $tempLongitude . '<br />';
-    echo $tempPhotoURL . '<br /><br />';
-
-    //Step 4 - Build the query string in a temp variable then actually insert the URL query string into
-    //$tempQuery = "INSERT INTO Feed VALUES('$tempUserID','$tempLatitude','$tempLongitude','$tempPhotoURL')";
-    //$result = mysql_query($tempQuery);
+    //Step 3 - Build the query string in a temp variable then actually insert the URL query string into
     $result = mysql_query("INSERT INTO Feed VALUES('$tempUserID','$tempLatitude','$tempLongitude','$tempPhotoURL')");
     
     if (!$result) die("Database INSERT failed: " . mysql_error());
 
-    echo "Did it work??<br />";
-    echo $result;
-    
     mysql_close($db_server);
     
 ?>
